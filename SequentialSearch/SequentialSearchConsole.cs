73989ee950b76ded38dd72f2267890d1f7d8601f
taylorgregory <taylor.gregory@connect.qut.edu.au>
2020-04-18 17:03:07 +1000

    sequential search console app

---
+++ SequentialSearch/SequentialSearchConsole.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SequentialSearch
+{
+    class SequentialSearchConsole
+    {
+        // Searches for a given value in a given array by sequential search
+        // Input: An array A[0..n-1] and a search key K
+        // Output: The index of the first element of A that matches K or -1 if there are no matching elements
+        static void Main()
+        {
+            // Defining the array to be searched from user input
+            Console.WriteLine("Type each element of the array that you would be searched. Separate each element with a single space. Press ENTER when complete.");
+            string input = Console.ReadLine();
+            string[] splitString = input.Split(" ");
+            int[] inArray = new int[splitString.Length];
+
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                inArray[i] = int.Parse(splitString[i]);
+            }
+
+            // Defining the search key from user input
+            Console.WriteLine("Type the value you would like to search for.");
+            int searchKey = int.Parse(Console.ReadLine());
+
+            // Sequential search algorithm
+            int j = 0;
+            while ((j < inArray.Length) && (inArray[j] != searchKey))
+            {
+                j += 1;
+            }
+            if (j < inArray.Length)
+            {
+                Console.WriteLine("The index of the search key is " + j.ToString());
+            }
+            else
+            {
+                Console.WriteLine("The search key does not appear in the given array");
+            }
+        }
+    }
+}
