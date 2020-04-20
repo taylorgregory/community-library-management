9e9a594e608b861261c955ece6319b5a9d16ea47
taylorgregory <taylor.gregory@connect.qut.edu.au>
2020-04-20 17:00:14 +1000

    setup for assignment

---
+++ LibraryManagement/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    class Member
+    {
+        //full name 
+        // residential address
+        // contact phone number
+
+        //information about the movie dvds tat are currently being borrowed))
+
+    }
+}
---
+++ LibraryManagement/Movie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Movie
+    {
+        // data members, data fields or characteristics
+        private string title;
+        private string[] starring; // made this an array because there could be several people?
+        private string director;
+        private int duration; //min
+        private enum Genre { Drama, Adventure, Family, Action, SciFi, Comedy, Animated, Thriller, Other };
+        private Genre genre;
+        private enum Classification { General, ParentalGuidance, Mature, MatureAccompanied };
+        private Classification classification;
+        private string releaseDate;
+
+        // constructor
+
+        // getters and setters
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        // **** not sure about array (starring)
+
+        public string Director
+        {
+            get { return director; }
+            set { director = value; }
+        }
+
+        public int Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+        // genre
+
+        // classification
+
+        public string ReleaseDate
+        {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+    }
+}
